83e2dfaa0e564c4ab8253687f49ea1656bdea549
+ <+>
2022-09-28 16:15:39 +0300

    updated

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+    BrowserRouter,
+    Routes, // instead of "Switch"
+    Route,
+  } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <div>
+    <NavBar />
+      <Routes>
+        <Route exact path="/about">
+        <About />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Routes>
+
+    </div>
+  );
+}
+
+export default App;
